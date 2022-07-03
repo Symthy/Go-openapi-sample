dd64db41301891d38a221dc1a70e73839c931ba1
Symthy <sympathia3104@gmail.com>
2022-07-04 00:24:01 +0900

    add: practice windows service (by kardianos/service)

---
+++ go-win-service/cmd/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/kardianos/service"
+	"github.com/natefinch/lumberjack"
+)
+
+var logger service.Logger
+
+type exarvice struct {
+	exit chan struct{}
+}
+
+func (e *exarvice) run() error {
+	log.Print("[lumberjack] Exarvice Start !!!")
+	logger.Info("[Service] Exarvice Start !!!")
+
+	ticker := time.NewTicker(5 * time.Second)
+	for {
+		select {
+		case tm := <-ticker.C:
+			log.Printf("[lumberjack] Still running at %v", tm)
+			logger.Infof("[Service] Still running at %v", tm)
+		case <-e.exit:
+			ticker.Stop()
+			log.Print("[lumberjack] Exarvice Stop ...")
+			logger.Info("[Service] Exarvice Stop ...")
+			return nil
+		}
+	}
+}
+
+func (e *exarvice) Start(s service.Service) error {
+	if service.Interactive() {
+		log.Print("[lumberjack] Running in terminal.")
+		logger.Info("[Service] Running in terminal.")
+	} else {
+		log.Print("[lumberjack] Running under service manager.")
+		logger.Info("[Service] Running under service manager.")
+	}
+	e.exit = make(chan struct{})
+
+	go e.run()
+	return nil
+}
+
+func (e *exarvice) Stop(s service.Service) error {
+	close(e.exit)
+	return nil
+}
+
+func main() {
+	svcConfig := &service.Config{
+		Name:        "Exarvice",
+		DisplayName: "Exarvice (Go Service Example)",
+		Description: "This is an example Go service.",
+	}
+	log.SetOutput(&lumberjack.Logger{
+		Filename: "C:\\work\\win_service_lumberjack.log",
+	})
+	log.Print("main kick")
+
+	// Create Exarvice service
+	program := &exarvice{}
+	s, err := service.New(program, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Setup the logger
+	errs := make(chan error, 5)
+	logger, err = s.Logger(errs)
+	if err != nil {
+		log.Fatal()
+	}
+
+	if len(os.Args) > 1 {
+		err = service.Control(s, os.Args[1])
+		if err != nil {
+			fmt.Printf("Failed (%s) : %s\n", os.Args[1], err)
+			return
+		}
+		fmt.Printf("Succeeded (%s)\n", os.Args[1])
+		return
+	}
+
+	// run in terminal
+	s.Run()
+}
---
+++ go-win-service/memo.md
@@ -0,0 +1,33 @@
+# memo
+
+golange で開発し、ビルドして生成した exe ファイルは、
+
+sc や [NSSM (the Non-Sucking Service Manager)](http://nssm.cc/) では、Windows サービス化はできても、起動ができない。
+
+以下によると、[kardianos/service](https://github.com/percona/kardianos-service) を使えばよいとのこと。
+
+[Cannot start a Go application exe as a windows services](https://stackoverflow.com/questions/35605238/cannot-start-a-go-application-exe-as-a-windows-services?newreg=af7591295de54a9d9525272feac3c9e9&answertab=createdasc#tab-top)
+
+[Windows サービスとして golang アプリケーション exe を起動できません](https://www.web-dev-qa-db-ja.com/ja/windows/windows%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9%E3%81%A8%E3%81%97%E3%81%A6golang%E3%82%A2%E3%83%97%E3%83%AA%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3exe%E3%82%92%E8%B5%B7%E5%8B%95%E3%81%A7%E3%81%8D%E3%81%BE%E3%81%9B%E3%82%93/823916534/amp/)
+
+詳細な理由までは分かっていないが、windows サービス化するためには、それに必要な処理が実装されている必要があるらしい。
+
+ref: [エラー 1053：カスタムサービスが開始されません](https://answers.microsoft.com/en-us/windows/forum/all/error-1053-custom-service-does-not-start/810fcf93-0cca-43cd-9e71-9050e7ee80ed)
+
+golang には、windows サービス化 に必要となる処理も含む準標準ライブラリ（golang.org/x/sys）があり、ご丁寧にサンプルコードもある（https://pkg.go.dev/golang.org/x/sys@v0.0.0-20220702020025-31831981b65f/windows/svc/example）
+
+[kardianos/service](https://github.com/percona/kardianos-service) は windows の処理に関しては上記ライブラリを使用しつつ、win, linux, mac 等でサービス化を可能とするための共通のインターフェースを提供している。
+
+# refs
+
+- kardianos/service 使用サンプル
+
+[Go 言語で Windows の Service を作成する](https://qiita.com/mako2kano/items/70e893b6c0fe178d5239) 2018/06
+
+[go で Windows service を作成する](https://qiita.com/yamasaki-masahide/items/c60e49a908c7927ca600) 2014/12
+
+[Go 言語で Windows,Linux の常駐システムを開発する](https://tech-blog.optim.co.jp/entry/2022/04/28/100000)
+
+- その他
+
+[Go 言語 - Windows 上でのプロセス存在チェック](https://blog.y-yuki.net/entry/2018/08/03/000000)
